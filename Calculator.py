841d248853092a27f1a49f39273c88b42303c9ee
PudingThePie <47710538+Axelthy@users.noreply.github.com>
2019-07-16 06:57:59 +0000

    Calculator Made By Me
    Of course it's not my idea, it's just testing bla bla

---
+++ Calculator.py
@@ -0,0 +1,59 @@
+import time
+print("system start")
+time.sleep(0.1)
+print("system start")
+time.sleep(0.1)
+print("system start")
+time.sleep(0.1)
+print("system start")
+time.sleep(0.1)
+print("system start")
+time.sleep(0.1)
+print( )
+time.sleep(0.1)
+print( )
+time.sleep(0.1)
+print( )
+time.sleep(0.1)
+print( )
+time.sleep(0.1)
+print("Hello Welcome to my calculator, made by Harun M. .")
+
+
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+print("")
+time.sleep(0.1)
+
+
+print("1 for adding")
+time.sleep(0.3)
+print("2 for subtracting")
+time.sleep(0.3)
+print("3 for multiply")
+time.sleep(0.3)
+print("4 for Divide")
+time.sleep(1)
+
+choice = int(input("Enter calculation type:"))
+if choice == 1 or 2 or 3 or 4 :
+   True
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+if choice == 1:
+   result = num1 + num2
+   print("Result = ", result)
+if choice == 2:
+   result = num1 - num2
+   print("Result = ", result)
+if choice == 3:
+   result = num1 * num2
+   print("Result = ", result)
+if choice == 4:
+     result = num1 / num2
+     print("Result = ", result)
+
